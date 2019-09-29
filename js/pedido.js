dbd174484268ba165274893216889fa196a88c7c
reingyar <yosdandclxv1@gmail.com>
2019-09-29 12:20:13 -0400

    jutsu 3000

--- js/pedido.js
+++ js/pedido.js
@@ -2,10 +2,15 @@ const arepa = new Carrito();
 const carrito = document.getElementById('carrito');
 const productos = document.getElementById('lista-productos');
 const listaProductos = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
 cargarEventos()
 
 function cargarEventos(){
     productos.addEventListener('click', (e)=>{arepa.comprarProducto(e)});
+
+    carrito.addEventListener('click', (e)=>{arepa.eliminarProducto(e)});
+
+    vaciarCarritoBtn.addEventListener('click', (e)=>{arepa.vaciarCarrito(e)});
 }
 
